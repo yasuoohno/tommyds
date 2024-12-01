53e3f57fa9a4d0ee1bf5c7f44891744f123d2b2b
Yasuo Ohno <yasuo.ohno@gmail.com>
2024-12-01 14:32:46 +0900

    fix compile issues with Visual Studio 2022

--- tommyds/tommytypes.h
+++ tommyds/tommytypes.h
@@ -35,22 +35,22 @@
 /* types */
 
 #include <stddef.h>
+#include <stdint.h>
 
 #ifdef _MSC_VER
-typedef unsigned tommy_uint32_t; /**< Generic uint32_t type. */
-typedef unsigned _int64 tommy_uint64_t; /**< Generic uint64_t type. */
+typedef uint32_t tommy_uint32_t; /**< Generic uint32_t type. */
+typedef uint64_t tommy_uint64_t; /**< Generic uint64_t type. */
 typedef size_t tommy_uintptr_t; /**< Generic uintptr_t type. */
 #ifdef _WIN64
 #define TOMMY_SIZE_BIT 64
-typedef unsigned _int64_t tommy_size_t; /**< Generic size_t type. */
-typedef _int64_t tommy_ssize_t; /**< Generic ssize_t type. */
+typedef uint64_t tommy_size_t; /**< Generic size_t type. */
+typedef int64_t tommy_ssize_t; /**< Generic ssize_t type. */
 #else
 #define TOMMY_SIZE_BIT 32
-typedef unsigned tommy_size_t; /**< Generic size_t type. */
-typedef int tommy_ssize_t; /**< Generic ssize_t type. */
+typedef uint32_t tommy_size_t; /**< Generic size_t type. */
+typedef int32_t tommy_ssize_t; /**< Generic ssize_t type. */
 #endif
 #else
-#include <stdint.h>
 typedef uint32_t tommy_uint32_t; /**< Generic uint32_t type. */
 typedef uint64_t tommy_uint64_t; /**< Generic uint64_t type. */
 typedef uintptr_t tommy_uintptr_t; /**< Generic uintptr_t type. */
